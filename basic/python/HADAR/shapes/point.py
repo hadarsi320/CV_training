aeac4a0072836440d470eed8575a6f0a5e6b4c6b
hadarsi320 <hadarsi320@gmail.com>
2023-04-16 17:11:14 +0300

    Point now inherits from Circle and does not receive a value for fill

--- basic/python/HADAR/shapes/point.py
+++ basic/python/HADAR/shapes/point.py
@@ -1,23 +1,15 @@
 import warnings
 
-import cv2 as cv
-
-from .basic_shape import BasicShape
+from .circle import Circle
 
 __all__ = ['Point']
 
 
-class Point(BasicShape):
+class Point(Circle):
     def __init__(self, p, **kwargs):
-        super(Point, self).__init__([p], **kwargs)
-        #TODO: why not inherit from circle?
-        if self.line_color is None: # TODO: again default?
-            raise ValueError('Line color must be passed to points')
+        super(Point, self).__init__([p], radius=1, fill_color=None, **kwargs)
         if self.fill_color is not None:
             warnings.warn('Fill color is meaningless for points')
-
-    def draw(self, canvas):
-        cv.circle(canvas, self.points[0], 1, self.line_color)
 
     def scale(self, scale, center):
         warnings.warn('Scaling does nothing on a point')
