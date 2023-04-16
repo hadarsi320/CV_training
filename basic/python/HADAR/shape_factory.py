29786c9299074f22de05036f101c1bfc4cd6d82d
hadarsi320 <hadarsi320@gmail.com>
2023-04-16 17:12:19 +0300

    Now using dynamic instantiation for basic shape creation and memoization for json composite creation

--- basic/python/HADAR/shape_factory.py
+++ basic/python/HADAR/shape_factory.py
@@ -1,9 +1,14 @@
-import json
+from copy import deepcopy
 
-from shapes import *
+import json
+import shapes
+from shapes import Composite
 
 
 class ShapeFactory:
+    def __init__(self):
+        self.composite_shapes = {}
+
     def create_shapes_from_list(self, list_of_shapes):
         shapes = []
         factory = ShapeFactory()
@@ -21,27 +26,25 @@ class ShapeFactory:
         return shape
 
     def create_basic_shape(self, shape, **kwargs):
-        shape = shape.lower()
-        if shape == 'line': #TODO: can you do it without defining the class names hard coded? (read about dynamic instantiation)
-            shape_obj = Line(**kwargs)
-        elif shape == 'circle':
-            shape_obj = Circle(**kwargs)
-        elif shape == 'point':
-            shape_obj = Point(**kwargs)
-        elif shape == 'triangle':
-            shape_obj = Triangle(**kwargs)
-        elif shape == 'rectangle':
-            shape_obj = Rectangle(**kwargs)
-        else:
+        try:
+            shape_class = getattr(shapes, shape)
+        except AttributeError:
             raise ValueError(f'Unknown shape: {shape}')
+        shape_obj = shape_class(**kwargs)
         return shape_obj
 
-    def create_composite_shape(self, **kwargs): #TODO: add shape dictionary (read about memoization)
+    def create_composite_shape(self, **kwargs):
         if 'shapes' in kwargs:
             shapes = self.create_shapes_from_list(kwargs.pop('shapes'))
         elif 'json' in kwargs:
-            list_of_shapes = json.load(open(kwargs.pop('json')))
-            shapes = self.create_shapes_from_list(list_of_shapes)
+            json_file = kwargs.pop('json')
+            if json_file in self.composite_shapes:
+                shapes = deepcopy(self.composite_shapes[json_file])
+            else:
+                list_of_shapes = json.load(open(json_file))
+                shapes = self.create_shapes_from_list(list_of_shapes)
+                self.composite_shapes[json_file] = deepcopy(shapes)
+                print('Created composite shape')
         else:
             raise ValueError('Composite must have either a "shapes" or a "json" field')
         composite = Composite(shapes, **kwargs)
